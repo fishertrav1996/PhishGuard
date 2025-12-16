3206ba52c3955342cc7223d7a1dbe968072285e1
fishertrav1996 <fishertrav@gmail.com>
2025-12-16 12:52:17 -0600

    Removed test URLS from orgs

--- orgs/urls.py
+++ orgs/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path('new', views.new_org_view, name='new_org'),
-    path('hello', views.hello_world_view, name='hello world'),
-    path('hello/<str:name>', views.hello_path_view, name='hello name'),
-    path('hello_query', views.hello_query_view, name='hello query'),
+   
 ]
